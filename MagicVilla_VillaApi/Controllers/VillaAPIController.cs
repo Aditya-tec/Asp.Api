7c0346719acee2c36c0a22f4206e6a4531cd2968
Aditya-tec <zenowing3@gmail.com>
2025-03-09 12:53:57 +0530

    31:35

---
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_VillaApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = " pool Villa " },
+                new Villa { Id = 2, Name = "beach Villa" },
+            };
+        }
+    }
+}
