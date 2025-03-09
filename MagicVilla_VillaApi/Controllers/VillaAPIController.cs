4230c4e0101304664210a3824b02b812ba5f1acc
Aditya-tec <zenowing3@gmail.com>
2025-03-09 18:20:24 +0530

    49:10

--- MagicVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -1,18 +1,34 @@
 ﻿using MagicVilla_VillaApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaApi.Models.Dto;
+using MagicVilla_VillaApi.Data;
+using System.Xml;
 
 namespace MagicVilla_VillaApi.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-        public IEnumerable<Villa> GetVillas()
+        [HttpGet]
+        public ActionResult <IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<Villa>
+            return Ok(VillaStore.villaList);
+        }
+    
+        [HttpGet("{id:int}")]
+        public ActionResult <VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
             {
-                new Villa { Id = 1, Name = " pool Villa " },
-                new Villa { Id = 2, Name = "beach Villa" },
-            };
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
         }
     }
 }
