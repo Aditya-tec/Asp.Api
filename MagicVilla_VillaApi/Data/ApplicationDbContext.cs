b061b370749269bdc5549cae8a31cdbee17446d2
Aditya-tec <zenowing3@gmail.com>
2025-03-10 16:21:48 +0530

    2:03:00

---
+++ MagicVilla_VillaApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MagicVilla_VillaApi.Models;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+
+    }
+}
