38cf071601b4253f9883f4f68c28e33fe499085c
Aditya kalambe <75620159+Aditya-tec@users.noreply.github.com>
2025-03-17 14:58:11 +0530

    Update Villa.cs

--- MagicVilla_VillaApi/Models/Villa.cs
+++ MagicVilla_VillaApi/Models/Villa.cs
@@ -15,7 +15,7 @@ namespace MagicVilla_VillaApi.Models
         public string ImageUrl { get; set; }
         public string Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; },
 
 
     }
